f9eac6584eb31b9fd7eea0afbf488f4b31880127
Zeltzin Romero <zeltzin.rom96@gmail.com>
2023-11-13 21:42:44 -0600

    Creation of page that shows people from the series Rick and Morty

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import imageRickMorty from "./img/rick-morty.png"
 import './App.css';
+import { useState } from "react";
+import Characters from "./components/Characters";
 
 function App() {
+
+  const [characteres, setCharacteres] = useState(null)
+
+  const reqApi = async () => {
+    const api = await fetch('https://rickandmortyapi.com/api/character')
+    const characterApi = await api.json()
+    setCharacteres(characterApi.results)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title">Rick & Morty</h1>
+        {characteres ? (
+          <Characters characteres={characteres} setCharacteres={setCharacteres}/>
+        ) : (
+          <>
+            <img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
+            <button onClick={reqApi} className="btn-search">Buscar Personaje</button>
+          </>
+        )}
       </header>
     </div>
   );
